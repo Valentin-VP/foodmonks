f40f51cbe066e214b3f6c4af208889759e6a56cb
Manuel <manu.biurrun@gmail.com>
2021-10-21 16:09:01 -0300

    switch button para apertura y cierre del restaurante

--- frontend/src/pages/restaurante/Home.js
+++ frontend/src/pages/restaurante/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import { Layout } from "../../components/Layout";
 import styled from "styled-components";
+import { Noti } from "../../components/Notification";
 // import ItemCard from "../../components/itemCard";
 // import prods from "../../productos";
 
@@ -13,14 +14,47 @@ const Styles = styled.div`
     font-weight: bold;
     padding-top: 30px;
   }
+
+  #aperturaCierreSwitch {
+    font-size: 25px;
+  }
+
+  #aperturaCierreLabel {
+    text-decoration: none;
+    font-size: 25px;
+    font-family: "Poppins", sans-serif;
+    color: #e87121;
+    font-weight: bold;
+  }
 `;
 
-export const Home = () => (
+function Home() {
+  const [estado, setEstado] = useState(true);
+
+  const onClick = () => {
+    setEstado(!estado);
+    if(estado) {
+      //llamo al backend con estado ABIERTO
+      Noti("ABIERTO!!");
+    } else {
+      //llamo al backend con estado CERRADO
+      Noti("CERRADO!!");
+    }
+  }
+
+  return (
   <Styles>
     <React.Fragment>
       <Layout>
+        <div class="form-check form-switch">
+          <input class="form-check-input" type="checkbox" id="aperturaCierreSwitch" onClick={onClick}></input>
+          <label class="form-check-label" for="aperturaCierreSwitch" id="aperturaCierreLabel">Indicar apertura/cierre</label>
+        </div>
         <h2 id="titulo">Esto es de un restaurante</h2>
       </Layout>
     </React.Fragment>
   </Styles>
-);
+  );
+}
+
+export default Home;
