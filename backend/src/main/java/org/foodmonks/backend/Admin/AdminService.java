a2750a0995d4c871e8e9b328cc8eff4fc6f63742
Esteban Petrone <luis.petrone@fing.edu.uy>
2021-10-27 01:53:45 -0300

    Se corrije encode en bd y se agrega Exception en Admin al usar un correo ya existente para otro usuario

--- backend/src/main/java/org/foodmonks/backend/Admin/AdminService.java
+++ backend/src/main/java/org/foodmonks/backend/Admin/AdminService.java
@@ -1,25 +1,32 @@
 package org.foodmonks.backend.Admin;
 
 
+import org.foodmonks.backend.Usuario.Exceptions.UsuarioExisteException;
+import org.foodmonks.backend.Usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AdminService {
 
+    private final PasswordEncoder passwordEncoder;
     private final AdminRepository adminRepository;
+    private final UsuarioRepository usuarioRepository;
 
     @Autowired
-    public AdminService(AdminRepository adminRepository) {
-        this.adminRepository = adminRepository;
+    public AdminService(AdminRepository adminRepository, PasswordEncoder passwordEncoder, UsuarioRepository usuarioRepository ) {
+        this.adminRepository = adminRepository; this.passwordEncoder = passwordEncoder; this.usuarioRepository = usuarioRepository;
     }
 
-    public void crearAdmin(String correo, String nombre, String apellido, String password) {
-        Admin admin = new Admin(correo, nombre, apellido, password, LocalDate.now());
+    public void crearAdmin(String correo, String nombre, String apellido, String password) throws UsuarioExisteException {
+        if (usuarioRepository.findByCorreo(correo) != null){
+            throw new UsuarioExisteException("Ya existe un Usuario con el correo " + correo);
+        }
+        Admin admin = new Admin(correo, nombre, apellido, passwordEncoder.encode(password),LocalDate.now());
         adminRepository.save(admin);
     }
 
