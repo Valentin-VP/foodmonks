7f5dce80d78dbfeb1f1b2545697b73e2b50a3697
Rodrigo <rcastro@programmer.net>
2021-11-03 12:05:29 -0300

    Scripts de automatizacion

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker build -t foodmonks-dev-backend .
-docker tag foodmonks-dev-backend:latest 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-dev-backend:dev-latest
-docker push 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-dev-backend:dev-latest
+docker push 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-dev-backend
+docker tag foodmonks-dev-backend:latest 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-dev-backend:$1
+docker push 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-dev-backend:$1
