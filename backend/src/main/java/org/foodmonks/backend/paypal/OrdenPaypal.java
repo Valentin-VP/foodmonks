3afb039db42b869751eefc7f8a386d264735a57c
Rodrigo <rcastro@programmer.net>
2021-11-05 01:10:25 -0300

    Creada clase OrdenPaypal Endpoint para el order request de Paypal

---
+++ backend/src/main/java/org/foodmonks/backend/paypal/OrdenPaypal.java
@@ -0,0 +1,12 @@
+package org.foodmonks.backend.paypal;
+
+import lombok.Data;
+
+import java.net.URI;
+
+@Data
+public class OrdenPaypal {
+    private final String orderId;
+    private final URI linkAprobacion;
+    private final URI linkDevolucion;
+}
--- backend/src/main/java/org/foodmonks/backend/paypal/PayPalController.java
+++ backend/src/main/java/org/foodmonks/backend/paypal/PayPalController.java
@@ -1,10 +1,46 @@
 package org.foodmonks.backend.paypal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Controller
 @RequestMapping("/api/v1/paypal")
 public class PayPalController {
-    
+
+    //private PayPalService payPalService;
+
+    @Autowired
+    public PayPalController() {
+        //agregar service
+    }
+
+    @PostMapping("/order/request")
+    public String orderRequest(@RequestParam Double total, HttpServletRequest request){
+        final URI callbackUrl = callbackUrl(request);
+        OrdenPaypal ordenPaypal = payPalService.orderRequest(total, callbackUrl);
+        return "redirect:"+ ordenPaypal.getLinkAprobacion();
+    }
+
+
+    private URI callbackUrl(HttpServletRequest request) {
+        try {
+            URI requestUri = URI.create(request.getRequestURL().toString());
+            return new URI(requestUri.getScheme(),
+                    requestUri.getUserInfo(),
+                    requestUri.getHost(),
+                    requestUri.getPort(),
+                    "/api/v1/paypal/orders/capture",
+                    null, null);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
