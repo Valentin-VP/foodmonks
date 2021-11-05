624c2f7a819169863373cb49d687d87b0a09629c
Rodrigo <rcastro@programmer.net>
2021-11-05 00:52:37 -0300

    Creacion de PayPalController

---
+++ backend/src/main/java/org/foodmonks/backend/paypal/PayPalController.java
@@ -0,0 +1,10 @@
+package org.foodmonks.backend.paypal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/paypal")
+public class PayPalController {
+    
+}
