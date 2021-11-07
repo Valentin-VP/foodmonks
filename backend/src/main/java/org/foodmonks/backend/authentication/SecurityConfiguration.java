4bb5ba39f9dd6ad50363dce33cdf6e18d3c4d868
Rodrigo <rcastro@programmer.net>
2021-11-07 03:40:10 -0300

    Permisos endpoint

--- backend/src/main/java/org/foodmonks/backend/authentication/SecurityConfiguration.java
+++ backend/src/main/java/org/foodmonks/backend/authentication/SecurityConfiguration.java
@@ -60,6 +60,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .antMatchers("/api/v1/cliente/eliminarCuenta").hasRole("CLIENTE")
                         .antMatchers("/api/v1/admin").hasRole("ADMIN")
+                        .antMatchers("/api/v1/paypal/order/**").hasRole("CLIENTE")
                         .anyRequest().authenticated())
                 .addFilterBefore(new AuthenticationFilter(customService, tokenHelper),
                         UsernamePasswordAuthenticationFilter.class);
