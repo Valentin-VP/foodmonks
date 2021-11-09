a31c75df097136787dd384ccab21ac43451af86c
Esteban Petrone <luis.petrone@fing.edu.uy>
2021-11-08 21:56:22 -0300

    Fixes en los Repositorys

--- backend/src/main/java/org/foodmonks/backend/Usuario/UsuarioRepository.java
+++ backend/src/main/java/org/foodmonks/backend/Usuario/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package org.foodmonks.backend.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository  extends JpaRepository<Usuario, String> {
 
     Usuario findByCorreo(String correo);
