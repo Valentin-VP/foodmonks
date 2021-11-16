95ae6e072e6f783ba7723f598b6fcd8b07326c37
Manuel <manu.biurrun@gmail.com>
2021-11-16 13:37:54 -0300

    arreglos en front de App.js, nada grave

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ import { Loading } from "./components/Loading";
 
 toast.configure(); //esto esta para poder enviar las notificaciones
 function App() {
-  const [tipoUser, setTipoUser] = useState("ROLE_CLIENTE");
+  const [tipoUser, setTipoUser] = useState(null);
   if (getToken() !== null) {
     fetchUserData()
       .then((response) => {
