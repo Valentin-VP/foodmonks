e84a963d982c2329315b2460a7cc02c18efdc60b
Rodrigo <rcastro@programmer.net>
2021-11-24 11:29:38 -0300

    Reestructura de archivos y env files Pruebas en cloud, env funcionando local

--- backend/build_prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t foodmonks-prod-backend .
-docker push 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-backend
-docker tag foodmonks-prod-backend:latest 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-backend:$1
-docker push 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-backend:$1
--- frontend/build_prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t foodmonks-prod-frontend .
-docker push 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-frontend
-docker tag foodmonks-prod-frontend:latest 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-frontend:$1
-docker push 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-frontend:$1
