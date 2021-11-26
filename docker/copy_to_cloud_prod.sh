9f7b9b1d7e5b0358fcf7fbdf39573229dfcf0590
Rodrigo <rcastro@programmer.net>
2021-11-26 02:58:26 -0300

    Preparacion ambiente produccion

--- docker/copy_to_cloud_prod.sh
+++ docker/copy_to_cloud_prod.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-docker save -o backend.tar 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-backend:latest
-docker save -o frontend.tar 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-frontend:latest
-scp backend.tar frontend.tar aws-docker:/home/ubuntu/docker_images
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 711621010839.dkr.ecr.us-east-2.amazonaws.com
+docker compose -f docker-compose.aws.prod.yml build --build-arg "BACKEND_DOCKER_IMAGE_TAG=${1:-latest} FRONTEND_DOCKER_IMAGE_TAG=${1:-latest}" #--no-cache
+docker push 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-backend:${1:-latest}
+docker push 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-frontend:${1:-latest}
+docker save -o backend.tar 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-backend:${1:-latest}
+docker save -o frontend.tar 711621010839.dkr.ecr.us-east-2.amazonaws.com/foodmonks-prod-frontend:${1:-latest}
+scp backend.tar frontend.tar aws-prod:/home/ubuntu/docker_images
