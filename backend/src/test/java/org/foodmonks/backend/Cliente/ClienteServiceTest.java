608799e97811a78c748778feabf2c9919e354432
Nicolas Mendez Mañana <nicolas.mendez.manana@fing.edu.uy>
2021-11-29 18:46:36 -0300

    listarPedidosRealizados (lol), listarMenus

--- backend/src/test/java/org/foodmonks/backend/Cliente/ClienteServiceTest.java
+++ backend/src/test/java/org/foodmonks/backend/Cliente/ClienteServiceTest.java
@@ -6,6 +6,7 @@ import org.foodmonks.backend.Direccion.Direccion;
 import org.foodmonks.backend.Direccion.DireccionService;
 import org.foodmonks.backend.Direccion.Exceptions.DireccionNumeroException;
 import org.foodmonks.backend.EmailService.EmailService;
+import org.foodmonks.backend.Menu.Menu;
 import org.foodmonks.backend.Menu.MenuConverter;
 import org.foodmonks.backend.Menu.MenuRepository;
 import org.foodmonks.backend.Menu.MenuService;
@@ -13,17 +14,22 @@ import org.foodmonks.backend.MenuCompra.MenuCompraService;
 import org.foodmonks.backend.Pedido.PedidoConverter;
 import org.foodmonks.backend.Pedido.PedidoService;
 import org.foodmonks.backend.Reclamo.ReclamoService;
+import org.foodmonks.backend.Restaurante.Exceptions.RestauranteNoEncontradoException;
+import org.foodmonks.backend.Restaurante.Restaurante;
 import org.foodmonks.backend.Restaurante.RestauranteService;
 import org.foodmonks.backend.Usuario.Exceptions.UsuarioExisteException;
 import org.foodmonks.backend.Usuario.Usuario;
 import org.foodmonks.backend.Usuario.UsuarioService;
+import org.foodmonks.backend.datatypes.CategoriaMenu;
 import org.foodmonks.backend.datatypes.EstadoCliente;
+import org.foodmonks.backend.datatypes.EstadoRestaurante;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.thymeleaf.TemplateEngine;
@@ -55,7 +61,8 @@ class ClienteServiceTest {
     @Mock RestauranteService restauranteService;
     @Mock MenuCompraService menuCompraService;
     @Mock MenuService menuService;
-    @Mock MenuConverter menuConverter;
+    @Spy
+    MenuConverter menuConverter;
     @Mock MenuRepository menuRepository;
     @Mock PedidoConverter pedidoConverter;
     @Mock EmailService emailService;
@@ -255,5 +262,106 @@ class ClienteServiceTest {
         assertThatThrownBy(()->clienteService.modificarDireccionCliente("dummy", 2L, jsonDireccion))
                 .isInstanceOf(ClienteNoExisteDireccionException.class)
                 .hasMessageContaining("La direccion actual ingresada no existe para el Cliente dummy");
+    }
+
+    @Test
+    void listarPedidosRealizados() throws ClienteNoEncontradoException {
+        Direccion dir1 = new Direccion(1234, "calle", "esquina", "detalles", "latitud", "longitud");
+        Cliente cliente1 =  new Cliente("nombreDelCliente",
+                "apellidoDelCliente",
+                "cliente1@gmail.com", passwordEncoder.encode("a"),
+                LocalDate.of(2020, 01, 01), 4.0f,10,
+                List.of(dir1), EstadoCliente.ACTIVO, null, null);
+        when(clienteRepository.findByCorreo(anyString())).thenReturn(cliente1);
+        clienteService.listarPedidosRealizados("","","","","","","","", "", "");
+        clienteService.listarPedidosRealizados("a","","","","","","","", "", "");
+        clienteService.listarPedidosRealizados("","FINALIZADO","","","","","","", "", "");
+        clienteService.listarPedidosRealizados("","asdasd","","","","","","", "", "");
+        clienteService.listarPedidosRealizados("","","a","","","","","", "", "");
+        clienteService.listarPedidosRealizados("","","","a","","","","", "", "");
+        clienteService.listarPedidosRealizados("","","","","PAYPAL","","","", "", "");
+        clienteService.listarPedidosRealizados("","","","","asdasd","","","", "", "");
+        clienteService.listarPedidosRealizados("","","","","","","2020-01-01,2021-01-01","", "", "");
+        clienteService.listarPedidosRealizados("","","","","","","2020-01-01T18:05:05,2021-01-01T19:05:05","", "", "");
+        clienteService.listarPedidosRealizados("","","","","","","","1,2", "", "");
+        clienteService.listarPedidosRealizados("","","","","","","","1a,b2", "", "");
+        clienteService.listarPedidosRealizados("","","","","","","","", "0", "");
+        clienteService.listarPedidosRealizados("","","","","","","","", "0.5", "");
+        clienteService.listarPedidosRealizados("","","","","","","","", "", "10");
+        clienteService.listarPedidosRealizados("","","","","","","","", "", "1.0");
+        when(clienteRepository.findByCorreo(anyString())).thenReturn(null);
+        assertThatThrownBy(()->clienteService.listarPedidosRealizados("dummy","","",
+                "","","","","", "",""))
+                .isInstanceOf(ClienteNoEncontradoException.class)
+                .hasMessageContaining("No existe el Cliente dummy");
+
+//        JsonObject jsonObject = new JsonObject();
+//        jsonObject.addProperty("currentPage", 0);
+//        jsonObject.addProperty("totalItems", 5);
+//        jsonObject.addProperty("totalPages", 1);
+//        jsonObject.addProperty("pedidos", );
+//        when(pedidoService.listaPedidosHistorico(restaurante1, EstadoPedido.FINALIZADO,
+//                MedioPago.PAYPAL, "", new LocalDateTime[2], new Float[2], 0, 5)).thenReturn(null);
+    }
+
+    @Test
+    void listarMenus() throws RestauranteNoEncontradoException {
+        Direccion dir1 = new Direccion(1234, "calle", "esquina", "detalles", "latitud", "longitud");
+        Restaurante restaurante1 = new Restaurante("nombreDelRestaurante",
+                "apellidoDelRestaurante", "restaurante1@gmail.com",
+                passwordEncoder.encode("a"), LocalDate.of(2020, 01, 01),
+                4.0F, 10, "NombreRestaurante", 123456L,
+                dir1, EstadoRestaurante.ABIERTO, 23487123,
+                "DescripcionRestaurante", "CuentaDePaypal", null);
+        Menu menu1 = new Menu("dummy",10.0f,"dummy",
+                true,0.0f,"dummy", CategoriaMenu.PIZZAS,restaurante1);
+        menu1.setId(1L);
+        Menu menu2 = new Menu("dummy",20.0f,"dummy",
+                true,25.0f,"dummy", CategoriaMenu.HAMBURGUESAS,restaurante1);
+        menu2.setId(2L);
+
+        when(restauranteService.obtenerRestaurante(anyString())).thenReturn(restaurante1);
+        when(menuRepository.findMenusByRestaurante(any(Restaurante.class))).thenReturn(List.of(menu1, menu2));
+        List<JsonObject> expectedResult = menuConverter.listaJsonMenu(List.of(menu1,menu2));
+//        List<JsonObject> expectedResult = new ArrayList<>();
+//        JsonObject jsonMenu = new JsonObject();
+//        jsonMenu.addProperty("id", "1");
+//        jsonMenu.addProperty("nombre", "dummy");
+//        jsonMenu.addProperty("price", "10.0");
+//        jsonMenu.addProperty("descripcion", "dummy");
+//        jsonMenu.addProperty("visible", true);
+//        jsonMenu.addProperty("multiplicadorPromocion", "0.0");
+//        jsonMenu.addProperty("imagen", "dummy");
+//        jsonMenu.addProperty("categoria", CategoriaMenu.PIZZAS.name());
+//        jsonMenu.addProperty("restaurante", "restaurante1@gmail.com");
+//        expectedResult.add(jsonMenu);
+//        jsonMenu = new JsonObject();
+//        jsonMenu.addProperty("id", "2");
+//        jsonMenu.addProperty("nombre", "dummy");
+//        jsonMenu.addProperty("price", "20.0");
+//        jsonMenu.addProperty("descripcion", "dummy");
+//        jsonMenu.addProperty("visible", true);
+//        jsonMenu.addProperty("multiplicadorPromocion", "25.0");
+//        jsonMenu.addProperty("imagen", "dummy");
+//        jsonMenu.addProperty("categoria", CategoriaMenu.HAMBURGUESAS.name());
+//        jsonMenu.addProperty("restaurante", "restaurante1@gmail.com");
+//        expectedResult.add(jsonMenu);
+        //when(menuConverter.listaJsonMenu(any())).thenReturn(null);
+        List<JsonObject> result = clienteService.listarMenus("dummy", "", null, null);
+        assertThat(result).isEqualTo(expectedResult);
+
+        // --------------------------------------------
+        when(menuService.listMenuRestauranteCategoria(any(), any())).thenReturn(null);
+        result = clienteService.listarMenus("dummy", "BEBIDAS", null, null);
+        assertThat(result).isEqualTo(null);
+        // --------------------------------------------
+        when(menuService.existeCategoriaMenu(any(), any())).thenReturn(true);
+        expectedResult = menuConverter.listaJsonMenu(List.of(menu1));
+        result = clienteService.listarMenus("dummy", "BEBIDAS", 0.0f, 10.0f);
+        assertThat(result).isEqualTo(expectedResult);
+        // --------------------------------------------
+        expectedResult = menuConverter.listaJsonMenu(List.of(menu2));
+        result = clienteService.listarMenus("dummy", "", 11.0f, 18.0f);
+        assertThat(result).isEqualTo(expectedResult);
     }
 }
