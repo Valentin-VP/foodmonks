3ea80366ea149904a0c24a5d2b84de274872253b
Nicolas Mendez Mañana <nicolas.mendez.manana@fing.edu.uy>
2021-12-02 16:53:05 -0300

    fix adminConfig con rootfoodmonks@gmail.com y comentado hardcodeo clienteConfig (+ .properties testing)

--- backend/src/main/java/org/foodmonks/backend/Cliente/ClienteConfig.java
+++ backend/src/main/java/org/foodmonks/backend/Cliente/ClienteConfig.java
@@ -22,10 +22,10 @@ public class ClienteConfig {
     @Bean
     CommandLineRunner commandLineRunnerCliente(ClienteRepository repository) {
         return args ->{
-            Cliente cliente =  new Cliente("nombreDelCliente",
-                    "apellidoDelCliente",
-                    "cliente@gmail.com", passwordEncoder.encode("cliente123"), LocalDate.now(), 5.0f,0, null, EstadoCliente.ACTIVO, null, null);
-            repository.saveAll(List.of(cliente));
+//            Cliente cliente =  new Cliente("nombreDelCliente",
+//                    "apellidoDelCliente",
+//                    "cliente@gmail.com", passwordEncoder.encode("cliente123"), LocalDate.now(), 5.0f,0, null, EstadoCliente.ACTIVO, null, null);
+//            repository.saveAll(List.of(cliente));
         };
     }
 
